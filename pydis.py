510dc1891c24f2b82ee0ab0d5314acc4f506bb79
James Davenport <jradavenport@gmail.com>
2015-03-03 15:34:03 -0800

    first crack at flat and bias combine

--- pydis.py
+++ pydis.py
@@ -51,9 +51,11 @@ from scipy.interpolate import UnivariateSpline
 from scipy.interpolate import SmoothBivariateSpline
 import scipy.signal
 
+#########################
 def gaus(x,a,b,x0,sigma):
     return a*np.exp(-(x-x0)**2/(2*sigma**2))+b
 
+#########################
 def spec_trace(img, nsteps=100):
     '''
     Aperture finding and tracing
@@ -101,6 +103,7 @@ def spec_trace(img, nsteps=100):
     my = ap_spl(mx)
     return my
 
+#########################
 # def sky_subtract(image, My, apwidth=5, skysep=30, skybin=30):
 #     skysubflux = np.zeros_like(My)
 #     for i in range(0,len(My)):
@@ -109,10 +112,13 @@ def spec_trace(img, nsteps=100):
 #         return skysubflux
 #
 #
-#
+##########################
 # def HeNeAr_fit(calimage, linelist):
 #     take a slice thru calimage, find the peaks
 #     trace the peaks vertically, allow curve, spline as before
+#
+#    disp_approx = hdu[0].header['DISPDW']
+#    wcen_approx = hdu[0].header['DISPWC']
 #
 #     fit against linelist
 #
@@ -120,20 +126,86 @@ def spec_trace(img, nsteps=100):
 #     treat the wavelenth solution as a surface
 #     return wavemap
 #
-#
+##########################
 # def mapwavelength(trace, wavemap):
 #     use the wavemap from the HeNeAr_fit routine to determine the wavelength along the trace
 #     return trace_wave
 #
-#
-# def flatcombine(flatlist):
-#
-#     return flat
-#
-# def biascombine(biaslist):
-#
-#     return bias
 
+#########################
+def biascombine(biaslist, trim=True):
+    # assume biaslist is a simple text file with image names
+    # e.g. ls flat.00*b.fits > bflat.lis
+    files = np.loadtxt(biaslist,dtype='string')
+
+    for i in range(0,len(files)):
+        hdu_i = fits.open(files[i])
+
+        if trim is False:
+            im_i = hdu_i[0].data
+        if trim is True:
+            datasec = hdu_i[0].header['DATASEC'][1:-1].replace(':',',').split(',')
+            d = map(float, datasec)
+            im_i = hdu_i[0].data[d[2]-1:d[3],d[0]-1:d[1]]
+
+        # create image stack
+        if (i==0):
+            all_data = im_i
+        elif (i>0):
+            all_data = np.dstack( (all_data, im_i) )
+        hdu_i.close(closed=True)
+
+    # do median across whole stack
+    bias = np.median(all_data, axis=2)
+
+    # write output to disk for later use
+    hduOut = fits.PrimaryHDU(bias)
+    hduOut.writeto('BIAS.fits',clobber=True)
+    return bias
+
+
+#########################
+def flatcombine(flatlist, bias, trim=True):
+    # read the bias in, BUT we don't know if it's the numpy array or file name
+    if isinstance(bias, str):
+        # read in file if a string
+        bias_im = fits.open(bias)[0].data
+    else:
+        # assume is proper array from biascombine function
+        bias_im = bias
+
+    # assume flatlist is a simple text file with image names
+    # e.g. ls flat.00*b.fits > bflat.lis
+    files = np.loadtxt(flatlist,dtype='string')
+
+    for i in range(0,len(files)):
+        hdu_i = fits.open(files[i])
+        if trim is False:
+            im_i = hdu_i[0].data - bias_im
+        if trim is True:
+            datasec = hdu_i[0].header['DATASEC'][1:-1].replace(':',',').split(',')
+            d = map(float, datasec)
+            im_i = hdu_i[0].data[d[2]-1:d[3],d[0]-1:d[1]] - bias_im
+
+        # assume a median scaling for each flat to account for possible different exposure times
+        if (i==0):
+            all_data = im_i / np.median(im_i)
+        elif (i>0):
+            all_data = np.dstack( (all_data, im_i / np.median(im_i)) )
+        hdu_i.close(closed=True)
+
+    # do median across whole stack
+    flat = np.median(all_data, axis=2)
+    # need other scaling?
+
+    # write output to disk for later use
+    hduOut = fits.PrimaryHDU(flat)
+    hduOut.writeto('FLAT.fits',clobber=True)
+    return flat
+
+
+#########################
+#########################
 # test data setup
 datafile = 'example_data/Gliese176.0052b.fits'
 flatfile = 'example_data/flat.0039b.fits'
@@ -147,13 +219,21 @@ d = map(float, datasec)
 
 img = hdu[0].data[d[2]-1:d[3],d[0]-1:d[1]]
 
-disp_approx = hdu[0].header['DISPDW']
-wcen_approx = hdu[0].header['DISPWC']
+#### test trace
+# plt.figure()
+# plt.imshow(np.log10(img), origin = 'lower')
+# plt.plot(np.arange(img.shape[1]), spec_trace(img),'k',lw=3)
+# plt.show()
 
+bias_done = biascombine('bbias.lis')
+flat_done = flatcombine('bflat.lis', bias_done)
+flat_done2 = flatcombine('bflat.lis', 'BIAS.fits')
 
 plt.figure()
-plt.imshow(np.log10(img), origin = 'lower')
-plt.plot(np.arange(img.shape[1]), spec_trace(img),'k',lw=3)
+plt.imshow( flat_done, origin='lower')
 plt.show()
 
+plt.figure()
+plt.imshow( flat_done2, origin='lower')
+plt.show()
 

6c5ed44851d7b66e413583c274237987ed221b4a
James Davenport <jradavenport@gmail.com>
2015-03-19 14:32:56 -0700

    update example, add kpnoextinct file

--- example_data/example.py
+++ example_data/example.py
@@ -1,8 +1,11 @@
-import sys
-sys.path.append('../')
-import pydis
+import spectra
 
+# This is a simple example of reducing a list of images (bdata.lis)
+# in a hands-free manner. A reduced, wavelength calibrated spectrum
+# should appear for each file when finished.
 
-pydis.autoreduce('bdata.lis','bflat.lis', 'bbias.lis',
-                 'HeNeAr.0028b.fits',
-                 trim=True, display=False)
+spectra.autoreduce('bdata.lis','bflat.lis', 'bbias.lis',
+                   'HeNeAr.0028b.fits',
+                   apwidth=5, skysep=1, skywidth=15,
+                   trace1=True, HeNeAr_interac=False,
+                   trim=True, display=False)

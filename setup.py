acb0cd039d94f88767a0b2ea3925327e8287a211
Miguel de Val-Borro <miguel@archlinux.net>
2015-11-12 18:22:45 -0500

    Provide a list of classifiers to categorize the project

--- setup.py
+++ setup.py
@@ -9,4 +9,23 @@ setup(
     license = 'MIT',
     url = 'https://github.com/jradavenport/pydis',
     packages = ['pydis'],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is the project?
+        'Development Status :: 3 - Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        # should match "license" above
+        'License :: OSI Approved :: MIT License',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )

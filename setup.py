e069dcb23cb97f6c3b7f88d263e69981fff8d70e
Miguel de Val-Borro <miguel@archlinux.net>
2015-11-12 18:14:58 -0500

    Add setup.py script for building package
    Fixes #46

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'pydis',
+    version = '1.0',
+    description = 'A simple longslit spectroscopy pipeline in Python',
+    author = 'James Davenport',
+    author_email = 'jradavenport@gmail.com',
+    license = 'MIT',
+    url = 'https://github.com/jradavenport/pydis',
+    packages = ['pydis'],
+)
